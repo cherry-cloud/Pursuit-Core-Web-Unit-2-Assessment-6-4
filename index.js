cee5a0a1ec72b3adfde634e97d8e9c1d04733e4c
Danielle R Cherry <daniellecherry@pursuit.org>
2019-12-18 21:48:06 -0500

    List items for user reviews working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector("form");
   let reviewMovieTextInput = document.querySelector("#reviewMovie");
+  let userReviews = document.querySelector(".userReviews")
   let submitBtn = document.querySelector("#submitBtn");
   let userSubmitReview = document.querySelector("ul");
   let movieInfo = document.querySelector(".movieInfo");
@@ -8,6 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener("submit", (event) => {
       event.preventDefault();
+      let userReviewList = document.createElement("li");
+      userReviewList.innerText = reviewMovieTextInput.value;
+      userReviews.appendChild(userReviewList);
+      reviewMovieTextInput.value = ""
   })
   
   const requestMovieInfo = async () => {
@@ -16,29 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
       movieOption = movies.data
 
-      debugger;
+      
     }catch(error) {
       console.log(error)
     }
     
       const selectMovieOptions = () => {//Jon's Review
       // have to get to the movie array to then have for loop, loop through and displauy the movies as options.
-        for (let i = 0; i <= 20; i++) {
+        for (let i = 1; i <= movieOption; i++) {
           let options = document.createElement("option");
-          options.innerText = `${i}`;
+          options.innerText = [i].title;
           options.value = i;
           select.appendChild(options)
         }
       }
 
-      const movieDescription = () => {//[i]= should be theindex number of the movie array maybe
+      const movieDescription = () => {//[i]= should be theindex number of the movie array maybe try for loop here as well
         selectMovieOptions();//If I can get this to loop though the movies and display in the select then I should be able to key into it to have the info show in the movie description... or maybe the requestMoviesOption async function
         let title = document.createElement("h3");
         let movieYear = document.createElement("p");
         let movieSynop = document.createElement("p");
-        title.innerText = ;//movies.data[i].title
-        movieYear.innerText = ;//movies.data[i].release_date
-        movieSynop.innerText = ;//movies.data[i].description
+        // title.innerText = ;//movies.data[i].title
+        // movieYear.innerText = ;//movies.data[i].release_date
+        // movieSynop.innerText = ;//movies.data[i].description
 
         movieInfo.appendChild(title);
         movieInfo.appendChild(movieYear);
@@ -48,6 +53,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
       }
   }
+
+
   requestMovieInfo();
+  // movieDescription();
+
 
 })
