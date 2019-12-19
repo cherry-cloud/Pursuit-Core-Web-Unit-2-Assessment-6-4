e843eac4837f94eb06f82baa4da9b42e3fef54bc
Danielle R Cherry <daniellecherry@pursuit.org>
2019-12-19 18:28:49 -0500

    selcted populated with undefined..

--- index.js
+++ index.js
@@ -20,22 +20,23 @@ document.addEventListener("DOMContentLoaded", () => {
       let movies = await axios.get(`https://ghibliapi.herokuapp.com/films`)
 
       movieOption = movies.data
-
+      
       
     }catch(error) {
       console.log(error)
     }
     
-      const selectMovieOptions = () => {//Jon's Review
+      const selectMovieOptions = async () => {//Jon's Review
       // have to get to the movie array to then have for loop, loop through and displauy the movies as options.
-        for (let i = 1; i <= movieOption; i++) {
+        for (let i = 0; i < movieOption.length; i++) {
           let options = document.createElement("option");
-          options.innerText = [i].title;
-          options.value = i;
+          options.innerText = [0].title;
+          options.value = [0].title;
           select.appendChild(options)
+          debugger;
         }
       }
-
+      selectMovieOptions()
       const movieDescription = () => {//[i]= should be theindex number of the movie array maybe try for loop here as well
         selectMovieOptions();//If I can get this to loop though the movies and display in the select then I should be able to key into it to have the info show in the movie description... or maybe the requestMoviesOption async function
         let title = document.createElement("h3");
